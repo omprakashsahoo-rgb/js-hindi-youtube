d68683e247f72884f3a2920dfcc8af78c6395186
omprakashsahoo-rgb <sahooomprakash395@gmail.com>
2025-07-10 11:46:43 +0000

    arrays done

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,32 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros); // => Merges two array
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros); // => It also merge two array
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+// flat method merge all array present inside an array
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+// Converts all elements into an array
