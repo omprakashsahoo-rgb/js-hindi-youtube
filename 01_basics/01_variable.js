d6c9b519819b9dc12d0c4ae293eac68ed9751ba4
omprakashsahoo-rgb <sahooomprakash395@gmail.com>
2025-10-08 17:06:50 +0000

    basics done

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -20,3 +20,5 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+              
+//Above statement is able to print all at once 
